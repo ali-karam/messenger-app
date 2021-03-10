8c5b22d15245526ce62e364ee69a9dde28a6e203
ali-karam <alikaram_93@hotmail.com>
2021-03-10 18:36:34 -0400

    Added jwt tokens and set up cookies

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,10 +1,25 @@
 const db = require('../models');
+const jwt = require('jsonwebtoken');
+
+const expirationTimeInDays = 30;
+
+generateToken = username => (
+    jwt.sign({ username }, process.env.SECRET_KEY, 
+        { expiresIn: 60 * 60 * 24 * expirationTimeInDays })
+);
+
+saveToken = (res, token) => (
+    res.cookie('token', token, 
+        { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * expirationTimeInDays})
+);
 
 exports.register = async function(req, res, next) {
     try {
         const user = await db.User.create(req.body);
-        const { id, username} = user;
-        return res.status(201).json({ username });
+        const { username} = user;
+        const token = generateToken(username);
+        saveToken(res, token);
+        return res.status(201).json({ username, token });
     } catch(err) {
         if(err.code === 11000) {
             err.message = 'Sorry, that username and/or email is taken'
@@ -16,10 +31,12 @@ exports.register = async function(req, res, next) {
 exports.login = async function(req, res, next) {
     try {
         const user = await db.User.findOne({ username: req.body.username });
-        const { id, username } = user;
+        const { username } = user;
         const isMatch = await user.comparePassword(req.body.password);
         if(isMatch) {
-            return res.status(200).json({ username });
+            const token = generateToken(username);
+            saveToken(res, token);
+            return res.status(200).json({ username, token });
         } else {
             return next({status: 400, message: 'Invalid username/password'});
         }
