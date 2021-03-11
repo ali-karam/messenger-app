b7db679b9dea94be1c975639650173034fbd1ae5
ali-karam <alikaram_93@hotmail.com>
2021-03-11 00:36:13 -0400

    Added unit tests for authentication

---
+++ server/tests/auth.test.js
@@ -0,0 +1,113 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const db = require('../models');
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+    _id: userOneId,
+    username: 'Test',
+    email: 'test@example.com',
+    password: 'password'
+};
+
+afterAll(async () => {
+    await mongoose.connection.close();
+});
+
+describe('/POST register', () => {
+    afterAll(async () => {
+        await db.User.deleteMany();
+    });
+
+    it('Should register a new user and return a token', async () => {
+        const response = await request(app)
+            .post('/auth/register')
+            .send({
+                ...userOne
+            })
+            .expect(201);
+        const user = await db.User.findById(userOneId);
+        expect(user).not.toBeNull();
+        expect(response.body.token).not.toBeNull();
+    });
+
+    it('Should not store an unhashed password', async () => {
+        const user = await db.User.findById(userOneId);
+        expect(user.password).not.toBe('password');
+    });
+
+    it('Should not register an existing user', async () => {
+        await request(app)
+            .post('/auth/register')
+            .send({
+                ...userOne
+            })
+            .expect(400);
+        const numUsers = await db.User.countDocuments({
+            username: userOne.username
+        });
+        expect(numUsers).toBe(1);
+    });
+
+    it('Should not register a user with a password less than 6 chars', async () => {
+        await request(app)
+            .post('/auth/register')
+            .send({
+                username: 'Bob',
+                email: 'Bob@example.com',
+                password: '12345'
+            })
+            .expect(400);
+    });
+});
+
+describe('/POST login', () => {
+    afterAll(async () => {
+        await db.User.deleteMany();
+    });
+
+    it('Should log in an existing user and return a token', async () => {
+        await new db.User(userOne).save();
+        const response = await request(app)
+            .post('/auth/login')
+            .send({
+                username: userOne.username,
+                password: userOne.password
+            })
+            .expect(200);
+        expect(response.body.token).not.toBeNull();
+    });
+
+    it('Should not log in a non-existing user', async () => {
+        await request(app)
+            .post('/auth/login')
+            .send({
+                username: 'bob',
+                password: '123456'
+            })
+            .expect(400);
+    });
+
+    it('Should not log in a user with the wrong password', async () => {
+        await request(app)
+            .post('/auth/login')
+            .send({
+                username: userOne.username,
+                password: 'thisisnotmypass'
+            })
+            .expect(400);
+    });
+
+    it('Should not log in a user with the wrong username', async () => {
+        await request(app)
+            .post('/auth/login')
+            .send({
+                username: 'test2',
+                password: userOne.password
+            })
+            .expect(400);
+    });
+});
