2474709f5bcb76a9379936bb46bf9937ef878c8b
ali-karam <alikaram_93@hotmail.com>
2021-03-14 00:53:42 -0400

    Fixed a bug where username had no initial value

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -186,6 +186,7 @@ export default function Register() {
             </Grid>
             <Formik
               initialValues={{
+                username: "",
                 email: "",
                 password: ""
               }}
