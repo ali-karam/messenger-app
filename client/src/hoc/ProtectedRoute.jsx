ecd4deda2b17615fe406c2f32614990a83c3eed2
Ali Karam <49033343+ali-karam@users.noreply.github.com>
2021-03-17 15:19:22 -0300

    Update ProtectedRoute.jsx
    Passed ProtectedRoute props to Route

--- client/src/hoc/ProtectedRoute.jsx
+++ client/src/hoc/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import AuthContext from '../context/auth-context';
 
-const ProtectedRoute = ({ component: Component }) => {
+const ProtectedRoute = ({ component: Component, ...rest }) => {
     const { user } = useContext(AuthContext);
-    return user ? <Route component={ Component } /> : <Redirect to='/login' />;
+    return user ? <Route component={ Component } {...rest} /> : <Redirect to='/login' />;
 };
 
 export default ProtectedRoute;
