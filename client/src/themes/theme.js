1fa469bbd6d1a47681649522334bc7d773d49f77
ali-karam <alikaram_93@hotmail.com>
2021-03-22 21:21:36 -0300

    Changed theme to match spec

--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -2,13 +2,14 @@ import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: '"Roboto"',
+    fontFamily: '"Open Sans"',
+    fontWeightRegular: 600,
     fontSize: 12,
     h1: {
       // could customize the h1 variant as well
     }
   },
   palette: {
-    primary: { main: "#DF1B1B" }
+    primary: { main: "#3A8DFF" }
   }
 });
