34391fe604e5683d1263bbf6d724e3025a38bd0d
ali-karam <alikaram_93@hotmail.com>
2021-03-25 01:11:40 -0300

    Created a helper function to reduce repeated code

--- server/handlers/conversations.js
+++ server/handlers/conversations.js
@@ -1,5 +1,13 @@
 const db = require('../models');
 
+const configOptions = (user) => { 
+    return {
+        path: 'users', 
+        select: 'username',  
+        match: { _id : { $ne : user } }
+    }
+};
+
 exports.startConversation = async function(req, res, next) {
     try {
         const currentUser = await db.User.findById(req.user);
@@ -12,11 +20,7 @@ exports.startConversation = async function(req, res, next) {
             throw new Error('You cannot start a conversation with yourself');
         }
         let conversation = await db.Conversation.initiateConversation(currentUser, otherUser);
-        conversation = await db.Conversation.populate(conversation, {
-            path: 'users', 
-            select: 'username',  
-            match: { _id : { $ne : req.user } }
-        });
+        conversation = await db.Conversation.populate(conversation, configOptions(req.user));
         res.status(201).json({ conversation });
     } catch(err) {
         return next({status: 400, message: err.message});
@@ -27,11 +31,8 @@ exports.getAllConversations = async function(req, res, next) {
     try {
         const currentUser = await db.User.findById(req.user);
         const conversations = await db.Conversation.find({ users: currentUser })
-            .populate({ 
-                path: 'users', 
-                select: 'username', 
-                match: { _id : { $ne : req.user } }
-            });
+            .populate(configOptions(req.user))
+            .sort({ updatedAt: 'desc' });
         res.status(200).json({ conversations });
     } catch(err) {
         return next({status: 400, message: err.message});
@@ -41,11 +42,7 @@ exports.getAllConversations = async function(req, res, next) {
 exports.getConversation = async function(req, res, next) {
     try {
         const conversation = await db.Conversation.findById(req.params.id)
-            .populate({
-                path: 'users', 
-                select: 'username', 
-                match: { _id : { $ne : req.user } }
-            });
+            .populate(configOptions(req.user));
         res.status(200).json({ conversation });
     } catch(err) {
         return next({status: 400, message: err.message});
