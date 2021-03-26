b7f9d53e38d1b36f3900cda861d882d6ca6d8289
ali-karam <alikaram_93@hotmail.com>
2021-03-25 22:16:11 -0300

    Added a middleware to ensure the correct user

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -15,3 +15,14 @@ exports.loginRequired = function(req, res, next) {
         return next({ status: 401, message: 'Please log in first'});
     }
 };
+
+exports.ensureCorrectUser = function(req, res, next) {
+    try {
+        if(req.user !== req.params.id) {
+            return next({ status: 401, message: 'Unauthorized'});
+        }
+        return next();
+    } catch(err) {
+        return next({ status: 401, message: 'Unauthorized'});
+    }
+};
