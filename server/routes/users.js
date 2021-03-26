c53dbbf9170c4349b50726ed3d16664c6cec2a85
ali-karam <alikaram_93@hotmail.com>
2021-03-25 23:10:14 -0300

    Renamed handler

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { findUser, getUser, upload, uploadAvatar } = require('../handlers/users');
+const { findUser, getUser, updateUser } = require('../handlers/users');
 const { ensureCorrectUser } = require('../middleware/auth');
+const { upload } = require('../middleware/fileUpload');
 
 router.get('/', findUser);
 router.route('/:id')
     .get(getUser)
-    .patch(ensureCorrectUser, upload.single('avatar'), uploadAvatar);
+    .patch(ensureCorrectUser, upload.single('avatar'), updateUser);
 
 
 module.exports = router;
