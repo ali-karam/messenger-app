cff34812b73569b525acaea3710b513e3c1b9207
ali-karam <alikaram_93@hotmail.com>
2021-04-04 22:34:39 -0300

    Created an image upload confirmation component

---
+++ client/src/components/Dashboard/ImgUploadConfirm/ImgUploadConfirm.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import imgUploadConfirmStyle from './ImgUploadConfirmStyle';
+
+const ImgUploadConfirm = React.forwardRef(({ src, yesClick, noClick }, ref) => {
+  const classes = imgUploadConfirmStyle();
+
+  return (
+    <div className={classes.root} ref={ref} tabIndex={-1}>
+      <img src={src ? URL.createObjectURL(src) : null} alt='Preview' className={classes.img} />
+      <Typography>Would you like to upload this image as your avatar?</Typography>
+      <div>
+        <Button variant='contained' color='primary' className={classes.button} onClick={yesClick}>
+          Yes
+        </Button>
+        <Button variant='contained' color='secondary' className={classes.button} onClick={noClick}>
+          No
+        </Button>
+      </div>
+    </div>
+  );
+});
+
+export default ImgUploadConfirm;
---
+++ client/src/components/Dashboard/ImgUploadConfirm/ImgUploadConfirmStyle.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const imgUploadConfirmStyle = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    backgroundColor: 'white',
+    outline: 'none',
+    margin: 'auto',
+    marginTop: 20,
+    width: 500,
+    height: 500,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '1em',
+    border: '4px solid black',
+    borderColor: theme.palette.primary.main
+  },
+  img: {
+    height: 300,
+    width: 300,
+    borderRadius: '50%',
+    marginBottom: 10
+  },
+  button: {
+    fontWeight: 'bold',
+    margin: '20px 10px 0 10px',
+    padding: '6px 40px'
+  }
+}));
+
+export default imgUploadConfirmStyle;
