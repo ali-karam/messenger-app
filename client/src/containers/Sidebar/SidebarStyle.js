e505c126cce408cd7debc08ef63105a8f584d894
ali-karam <alikaram_93@hotmail.com>
2021-04-07 16:38:23 -0300

    Removed unused class

--- client/src/containers/Sidebar/SidebarStyle.js
+++ client/src/containers/Sidebar/SidebarStyle.js
@@ -12,16 +12,6 @@ const sidebarStyle = makeStyles((theme) => ({
     marginLeft: 10,
     maxHeight: '100vh'
   },
-  searchBar: {
-    margin: '12px 0',
-    width: '90%',
-    backgroundColor: '#e7eef8',
-    padding: 10,
-    boxShadow: '1px 1px 2px #faf7f7',
-    height: '45px',
-    borderRadius: '0.4em',
-    fontSize: 13
-  },
   conversations: {
     width: '90%',
     overflow: 'scroll',
